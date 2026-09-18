d508088c8cbae25a23b35eacd397d6c534d186ec
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-18 18:50:14 -0300

    atk: Match `SoundPlayer::StopAllSound`

--- src/NintendoWare/atk/atk_SoundPlayer.cpp
+++ src/NintendoWare/atk/atk_SoundPlayer.cpp
@@ -15,4 +15,11 @@ SoundPlayer::SoundPlayer(detail::OutputAdditionalParam* pParam) : m_pOutputAddit
 }
 #endif
 
+void SoundPlayer::StopAllSound(int fadeFrames) {
+    for (auto itr{m_SoundList.begin()}; itr != m_SoundList.end();) {
+        auto curItr{itr++};
+        curItr->Stop(fadeFrames);
+    }
+}
+
 }  // namespace nn::atk
