582b7439daa341dea009a53dd3b363a4b8c21465
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-19 18:39:10 -0300

    atk: Match `SoundArchiveManager::Initialize`

--- src/NintendoWare/atk/detail/atk_SoundArchiveManager.cpp
+++ src/NintendoWare/atk/detail/atk_SoundArchiveManager.cpp
@@ -6,4 +6,13 @@ SoundArchiveManager::SoundArchiveManager() = default;
 
 SoundArchiveManager::~SoundArchiveManager() = default;
 
+void SoundArchiveManager::Initialize(const SoundArchive* pSoundArchive,
+                                     const SoundDataManager* pSoundDataManager) {
+    m_pMainSoundArchive = pSoundArchive;
+    m_pMainSoundDataManager = pSoundDataManager;
+    m_ContainerList.Clear();
+    m_pCurrentSoundArchive = m_pMainSoundArchive;
+    m_pCurrentSoundDataManager = m_pMainSoundDataManager;
+}
+
 }  // namespace nn::atk::detail
