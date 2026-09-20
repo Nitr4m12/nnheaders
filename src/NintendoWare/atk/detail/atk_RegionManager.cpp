c9a493ca1f963521bc28af2e550708f441c164a2
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-20 16:05:24 -0300

    atk: Match `RegionManager::IsPreparedForRegionJump`

--- src/NintendoWare/atk/detail/atk_RegionManager.cpp
+++ src/NintendoWare/atk/detail/atk_RegionManager.cpp
@@ -5,10 +5,19 @@ namespace nn::atk::detail {
 void RegionManager::Initialize() {
     m_IsRegionInfoEnabled = false;
     m_IsRegionIndexCheckEnabled = false;
+#if NN_WARE_VER >= NN_MAKE_VER(4, 0, 0)
     m_IsRegionInitialized = false;
+#endif
     m_StreamRegionCallbackFunc = nullptr;
     m_StreamRegionCallbackArg = nullptr;
     m_AdpcmContextForStartOffsetFrame = 0xffffffff;
+}
+
+bool RegionManager::IsPreparedForRegionJump() const {
+    if (!m_IsRegionInfoEnabled)
+        return false;
+
+    return m_StreamRegionCallbackFunc != nullptr;
 }
 
 }  // namespace nn::atk::detail
