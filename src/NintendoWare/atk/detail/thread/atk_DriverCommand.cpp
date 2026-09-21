9ba993b6bc877584f661e03d414f5682bee32644
Nitr4m12 <67806925+Nitr4m12@users.noreply.github.com>
2026-09-21 15:37:41 -0300

    atk: Match `DriverCommand::GetInstance`

--- src/NintendoWare/atk/detail/thread/atk_DriverCommand.cpp
+++ src/NintendoWare/atk/detail/thread/atk_DriverCommand.cpp
@@ -1,1 +1,12 @@
+#include <nn/atk/atk_DriverCommand.h>
 
+namespace nn::atk::detail {
+
+DriverCommand& DriverCommand::GetInstance() {
+    static DriverCommand instance;
+    return instance;
+}
+
+DriverCommand::DriverCommand() = default;
+
+}  // namespace nn::atk::detail
